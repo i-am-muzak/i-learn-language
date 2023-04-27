87edd37873be6b3fd3fed1602f861d693a58915d
i-am-muzak <hasan.muzak@hotmail.com>
2023-04-27 15:55:15 +0300

    frontend fix

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container mx-auto border p-4 text-center">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aliquam amet, animi assumenda aut consequatur dolore ea eius et hic iusto maiores minima molestias, necessitatibus neque numquam quis suscipit voluptas.
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+
+<style lang="scss" scoped>
+div {
+  color: #bb1515;
+}
+</style>
