b85398d53898486c7c0118b93c1d754af2869710
Hasan Muzak <hasan@apps.com.tr>
2023-04-29 16:56:12 +0300

    typo

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container mx-auto">
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aliquam amet, animi assumenda aut consequatur dolore ea eius et hic iusto maiores minima molestias, necessitatibus neque numquam quis suscipit voluptas.
+    Ne diin la
   </div>
 </template>
 
